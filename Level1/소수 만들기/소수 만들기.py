040973683aa09bd42f0ac9bdb0badc819d8c0c54
Yang Jaeho <42399580+SGTYang@users.noreply.github.com>
2021-07-17 21:27:25 +0900

    Update 소수 만들기.py

--- Level1/소수 만들기/소수 만들기.py
+++ Level1/소수 만들기/소수 만들기.py
@@ -15,16 +15,3 @@ def prime(number):
     
     for i in range(2,Math.sqrt(number)):
         if(number%i==0)
-'''boolean prime(int n){
-        if(n==0 || n==1){
-        
-        
-            return false;
-        }
-        for(int i=2; i<=(int)Math.sqrt(n); i++){
-            if(n%i==0){
-                return false;
-            }
-        }
-        return true;
-    }'''
