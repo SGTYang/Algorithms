15aa64cc710a67cdb4fbef8be3272134436c6810
Yang Jaeho <42399580+SGTYang@users.noreply.github.com>
2021-07-18 10:31:30 +0900

    Update 소수 만들기.py

--- Level1/소수 만들기/소수 만들기.py
+++ Level1/소수 만들기/소수 만들기.py
@@ -1,17 +1,26 @@
-import Math
+import math
 def solution(nums):
+
     answer = 0
-    
+    arry = []
+
     for i in range(len(nums)):
         for j in range(i+1,len(nums)):
             for k in range(j+1,len(nums)):
-                print(nums[i]+nums[j]+nums[k])
+                arry.append(nums[i]+nums[j]+nums[k])
+
+    for i in range(len(arry)):
+        if prime(arry[i]):
+            answer+=1
 
     return answer
 
 def prime(number):
-    if(number == 0 or number == 1):
-        return false
-    
-    for i in range(2,Math.sqrt(number)):
-        if(number%i==0)
+    if number == 0 or number == 1:
+        return False
+
+    for i in range(2,int(math.sqrt(number))+1):
+        if number%i == 0:
+            return False 
+    return True
+
