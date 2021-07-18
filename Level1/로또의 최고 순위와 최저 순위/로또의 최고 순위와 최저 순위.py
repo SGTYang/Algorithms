b87e260737b3bca533eece300857bd0b4789c9b2
Yang Jaeho <42399580+SGTYang@users.noreply.github.com>
2021-07-18 16:25:13 +0900

    Update 로또의 최고 순위와 최저 순위.py

--- Level1/로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
+++ Level1/로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
@@ -1,33 +1,28 @@
 def solution(lottos, win_nums):
     answer = []
-    
-    switch(n):
-        case 6: 
-            rank = 1
-            break
-        case 5:
-            rank = 2
-            break
-        case 4:
-            rank = 3
-            break
-        case 3:
-            rank = 4
-            break
-        case 2:
-            rank = 5
-            break
-            
-    set_lottos = set(lottos)
-    set_win_nums = set(win_nums)
-    
-    print(set_lottos - set_win_nums)
-    
+    answer.append(determine(max_num(lottos, win_nums)))
+    answer.append(determine(min_num(lottos, win_nums)))
     return answer
 
+def determine(number):
+    if number == 6:
+        return 1
+    elif number == 5:
+        return 2
+    elif number == 4:
+        return 3
+    elif number == 3:
+        return 4
+    elif number == 2:
+        return 5
+    else:
+        return 6
 def min_num(lottos, win_nums):
     set_lottos = set(lottos)
     set_win_nums = set(win_nums)
-    
-    print(set_lotots - set_win_nums)
-    return set_lottos
+    return len(set_win_nums)-len(set_win_nums - set_lottos)
+
+def max_num(lottos, win_nums):
+    set_lottos = set(lottos)
+    set_win_nums = set(win_nums)
+    return len(set_win_nums)-len(set_win_nums - set_lottos)+lottos.count(0)
