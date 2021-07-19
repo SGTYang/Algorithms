78c255efb62164641c395f7a6807510a5ad6f825
Yang Jaeho <42399580+SGTYang@users.noreply.github.com>
2021-07-19 10:01:38 +0900

    Update Two Sum.py
    Discussion을 참고하여 좀더 빠른 코드로 바꿨습니다.

--- LeetCode/Two Sum/Two Sum.py
+++ LeetCode/Two Sum/Two Sum.py
@@ -1,9 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        answer = []
-        for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                if nums[j] == target - nums[i]:
-                    answer.append(i)
-                    answer.append(j)
-        return answer
+        check = {}
+        for i,number in enumerate(nums):
+            rest = target - number
+            if rest in check:
+                return [i, check[rest]]
+            else:
+                check[number] = i
