5720dbb286554c113391f1da23542e54a6ace4cc
Yang Jaeho <42399580+SGTYang@users.noreply.github.com>
2021-07-19 11:17:28 +0900

    Delete LeetCode/Two Sum directory

--- LeetCode/Two Sum/Two Sum.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        check = {}
-        for i,number in enumerate(nums):
-            rest = target - number
-            if rest in check:
-                return [i, check[rest]]
-            else:
-                check[number] = i
