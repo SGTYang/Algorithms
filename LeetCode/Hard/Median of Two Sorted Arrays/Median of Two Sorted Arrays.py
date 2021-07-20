5d970f749a71e557fc44a6ca71a797b858e6cfee
Yang Jaeho <42399580+SGTYang@users.noreply.github.com>
2021-07-20 15:29:07 +0900

    Update Median of Two Sorted Arrays.py

--- LeetCode/Hard/Median of Two Sorted Arrays/Median of Two Sorted Arrays.py
+++ LeetCode/Hard/Median of Two Sorted Arrays/Median of Two Sorted Arrays.py
@@ -1,1 +1,13 @@
-def
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        
+        new_arry = nums1+nums2
+        
+        new_arry.sort()
+        
+        if len(new_arry)%2 != 0:
+            index = len(new_arry)//2
+            return new_arry[index]
+        else:
+            index = len(new_arry)//2
+            return (new_arry[index-1]+new_arry[index])/2 
