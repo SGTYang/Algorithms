7e654dc56f7a8e4027a90c339248963fcde88aad
Yang Jaeho <42399580+SGTYang@users.noreply.github.com>
2021-07-21 13:33:43 +0900

    Update Longest Palindromic Substring.py

--- LeetCode/Medium/Longest Palindromic Substring/Longest Palindromic Substring.py
+++ LeetCode/Medium/Longest Palindromic Substring/Longest Palindromic Substring.py
@@ -1,1 +1,15 @@
-def
+def longestPalindrome(self, s):
+    res = ""
+    for i in xrange(len(s)):
+        tmp = self.helper(s, i, i)
+        if len(tmp) > len(res):
+            res = tmp
+        tmp = self.helper(s, i, i+1)
+        if len(tmp) > len(res):
+            res = tmp
+    return res
+
+def helper(self, s, l, r):
+    while l >= 0 and r < len(s) and s[l] == s[r]:
+        l -= 1; r += 1
+    return s[l+1:r]
