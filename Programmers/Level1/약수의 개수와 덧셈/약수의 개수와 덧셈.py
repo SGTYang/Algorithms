91e232801461b7e55acb236bb4e2c45a0f1f7e67
Yang Jaeho <42399580+SGTYang@users.noreply.github.com>
2021-07-21 15:22:41 +0900

    Update 약수의 개수와 덧셈.py

--- Programmers/Level1/약수의 개수와 덧셈/약수의 개수와 덧셈.py
+++ Programmers/Level1/약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -1,1 +1,17 @@
-def
+def solution(left, right):
+    answer = 0
+    for i in range(left, right+1):
+        cnt = 1
+        tmp = 0
+        for j in range(1, i//2 + 1):
+            if i%j == 0:
+                cnt += 1
+
+        if cnt%2 != 0:
+            tmp = i*(-1)
+        else:
+            tmp = i
+            
+        answer += tmp
+        
+    return answer
