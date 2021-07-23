f34e3d0052ace93f77fbb5fd7dd24ae398573b1c
Yang Jaeho <42399580+SGTYang@users.noreply.github.com>
2021-07-23 17:07:27 +0900

    Update 에라토스테네스의 체.py

--- 에라토스테네스의 체/에라토스테네스의 체.py
+++ 에라토스테네스의 체/에라토스테네스의 체.py
@@ -1,4 +1,6 @@
 def finding_prime(n):
-    num_set = set(2:range(n)+1)
-    if i in num_set:
-        num_set -= set(range(i*2,n+1,i))
+    num_set = set(range(2,n+1))
+    
+    for i in range(2,n+1):
+        if i in num_set:
+            num_set -= set(range(i*2,n+1,i))
