f0e3b5b1dfe037d1f750b54197daa0c53a132895
Yang Jaeho <42399580+SGTYang@users.noreply.github.com>
2021-07-23 11:52:18 +0900

    Create 두 정수 사이의 합.java

---
+++ Programmers/Level1/두 정수 사이의 합/두 정수 사이의 합.java
@@ -0,0 +1,15 @@
+class Solution {
+    public long solution(int a, int b) {
+        long answer = 0;
+        
+    if(a>b){
+        int c=a;
+        a=b;
+        b=c;
+    }
+    for(int i=a; i<=b; i++){
+        answer=answer+i;
+        }
+    return answer;
+    }
+}
