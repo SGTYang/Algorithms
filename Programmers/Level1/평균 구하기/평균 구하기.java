aa24b2a33ad8f3fea10b1c456c21ff48c4928195
Yang Jaeho <42399580+SGTYang@users.noreply.github.com>
2021-07-25 17:16:54 +0900

    Create 평균 구하기.java

---
+++ Programmers/Level1/평균 구하기/평균 구하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public double solution(int[] arr) {
+        double answer = 0;
+        int sum=0;
+        for(int i=0; i<arr.length;i++){
+            sum+=arr[i];
+        }
+        answer = sum*(1.0)/arr.length;
+        return answer;
+    }
+}
