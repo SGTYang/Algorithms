7e837649c17bae218baf99d976594260e0d1d54a
Yang Jaeho <42399580+SGTYang@users.noreply.github.com>
2021-07-26 13:07:52 +0900

    Create 짝지어 제거하기.py

---
+++ Programmers/Level1/짝지어 제거하기/짝지어 제거하기.py
@@ -0,0 +1,17 @@
+def solution(s): 
+    
+    arry = []
+    
+    for i in s:
+        
+        if len(arry) == 0: 
+            arry.append(i)
+        elif arry[-1] == i: 
+            arry.pop()
+        else: 
+            arry.append(i)
+            
+    if len(arry) == 0: 
+        return 1
+    else: 
+        return 0
