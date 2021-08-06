42af20d139dae818ee31fac64f44602ba45daf9b
Yang Jaeho <42399580+SGTYang@users.noreply.github.com>
2021-08-06 10:18:42 +0900

    Update Palindrome Number.py

--- LeetCode/Easy/Palindrome Number/Palindrome Number.py
+++ LeetCode/Easy/Palindrome Number/Palindrome Number.py
@@ -1,10 +1,9 @@
-import math 
-
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        orig = x
-        back_x = 0
-        while x > 0:
-            back_x = (back_x * 10) + (x % 10)
-            x = x // 10
-        return orig == back_x
+        x = list(str(x))
+
+        for i in range(len(x)//2):
+            if x[i] != x[len(x)-i-1]:
+                return False
+
+        return True
