50db3afda1e0e68d88daed75edabc2d6c29aa576
Yang Jaeho <42399580+SGTYang@users.noreply.github.com>
2021-08-23 22:31:52 +0900

    Create 소수 찾기.py

---
+++ Programmers/Level2/소수 찾기/소수 찾기.py
@@ -0,0 +1,25 @@
+from itertools import permutations, combinations
+import math
+def solution(numbers):
+    answer = 0
+    permu = []
+    for i in range(1,len(numbers)+1):
+        tmp = list((permutations(numbers, i)))
+        [permu.append(int(k)) for k in ["".join(j) for j in tmp]]
+    permu = set(permu)
+    for i in permu:
+        if check_prime(i):
+            answer +=1
+    return answer
+
+def check_prime(n):
+    if n == 0 or n ==1:
+        return False
+    if n == 2 or n ==3 :
+        return True
+    for i in range(2,int(math.sqrt(n))+1):
+        if n%i == 0:
+            return False
+        
+    return True
+        
