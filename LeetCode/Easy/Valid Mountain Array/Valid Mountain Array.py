189b146e260859ea5ee23aa7c2544aff9d1f2406
Yang Jaeho <42399580+SGTYang@users.noreply.github.com>
2021-09-06 17:10:47 +0900

    Create Valid Mountain Array.py

---
+++ LeetCode/Easy/Valid Mountain Array/Valid Mountain Array.py
@@ -0,0 +1,24 @@
+class Solution:
+    def validMountainArray(self, arr: List[int]) -> bool:
+        if len(arr)<3:
+            return False
+        val = arr[0]
+        decrease = False
+        increase = False
+        for i in range(1,len(arr)):
+            if decrease:
+                if val<=arr[i]:
+                    return False
+            else:
+                if val>arr[i]:
+                    decrease = True
+                elif val == arr[i]:
+                    return False
+                else:
+                    increase = True
+            val = arr[i]
+            
+        if decrease and increase:
+            return True
+        else:
+            return False
