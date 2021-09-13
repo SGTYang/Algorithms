7017bf2b485d89a8c6162b770490f3fdb1fa4973
Yang Jaeho <42399580+SGTYang@users.noreply.github.com>
2021-09-13 10:23:43 +0900

    Update Intersection of Two Arrays II.py

--- LeetCode/Easy/Intersection of Two Arrays II/Intersection of Two Arrays II.py
+++ LeetCode/Easy/Intersection of Two Arrays II/Intersection of Two Arrays II.py
@@ -1,17 +1,12 @@
+from collections import Counter
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        dic = {}
-        
-        for i in nums1:
-            if i not in dic:
-                dic[i] = 0
-            dic[i] += 1
-        
+        dic = Counter(nums1)
+  
         answer = []
         
         for i in nums2:
             if i in dic and dic[i] >0:
                 dic[i] -= 1
                 answer.append(i)
-                
         return answer
