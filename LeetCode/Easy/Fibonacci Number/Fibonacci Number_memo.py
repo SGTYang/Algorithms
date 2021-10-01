57c0ae328f933089591a21e9adaee880d5334ca3
Yang Jaeho <42399580+SGTYang@users.noreply.github.com>
2021-10-01 10:29:39 +0900

    Create Fibonacci Number_memo.py

---
+++ LeetCode/Easy/Fibonacci Number/Fibonacci Number_memo.py
@@ -0,0 +1,12 @@
+class Solution:
+    def fib(self, n: int, memo = {}) -> int:
+        
+        if n in memo:
+            return memo[n]
+        if n ==0:
+            return 0
+        if n<3:
+            return 1
+        
+        memo[n] = self.fib(n-1,memo)+self.fib(n-2,memo)
+        return memo[n]
