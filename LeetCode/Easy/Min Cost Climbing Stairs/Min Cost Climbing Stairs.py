5f2693b5bb7bcf0c3e76d8298cd8a925ea203539
Yang Jaeho <42399580+SGTYang@users.noreply.github.com>
2021-10-05 11:34:37 +0900

    Create Min Cost Climbing Stairs.py

---
+++ LeetCode/Easy/Min Cost Climbing Stairs/Min Cost Climbing Stairs.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        
+        length = len(cost)
+        
+        if length ==2:
+            return min(cost)
+        
+        dp = [0]*length
+        
+        dp[0], dp[1] = cost[0], cost[1]
+        
+        for i in range(2,length):
+            dp[i] = min(dp[i-2]+cost[i],dp[i-1]+cost[i])
+            
+        return min(dp[-1],dp[-2])
