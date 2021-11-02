bc1ba49fcc25dd8f545aceb3b7f42aff9067a326
Yang Jaeho <42399580+SGTYang@users.noreply.github.com>
2021-11-02 17:13:36 +0900

    Create 올바른 괄호.py

---
+++ Programmers/Level2/올바른 괄호/올바른 괄호.py
@@ -0,0 +1,19 @@
+def solution(s):
+    answer = True
+    open_parenthese = ['(']
+    close_parenthese = [')']
+    
+    stack = []
+    
+    for i in range(len(s)):
+        if s[i] == '(':
+            stack.append(s[i])
+        else:
+            if len(stack) != 0:
+                stack.pop()
+            else:
+                return False
+    if len(stack) !=0:
+        return False
+    
+    return True
