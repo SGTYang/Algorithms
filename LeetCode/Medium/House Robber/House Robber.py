a3a9e6046fa4b96e007763d5ab04fa47accecb9f
Yang Jaeho <42399580+SGTYang@users.noreply.github.com>
2021-11-12 15:23:56 +0900

    Update House Robber.py

--- LeetCode/Medium/House Robber/House Robber.py
+++ LeetCode/Medium/House Robber/House Robber.py
@@ -1,16 +1,17 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         
-        dp = [0]*len(nums)
-        
-        if len(nums) == 1:
+        dp= [0]*len(nums)
+        if len(nums)==1:
             return nums[0]
-        if len(nums) ==2:
+        if len(nums)==2:
             return max(nums[0],nums[1])
         
-        dp[0], dp[1] = nums[0],max(nums[0],nums[1])
+        dp[0],dp[1],dp[2] = nums[0],nums[1],nums[2]
         
         for i in range(2,len(nums)):
-            dp[i] = max(dp[i-2]+nums[i],dp[i-1])
+            dp[i] = max(dp[i-2],dp[i-3])+nums[i]
             
+        print(dp)
+        
         return max(dp[-1],dp[-2])
