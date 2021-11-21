dc119c137f7648b881ff0f3b75c3fc3b4ccbd009
Yang Jaeho <42399580+SGTYang@users.noreply.github.com>
2021-11-21 18:47:26 +0900

    Update House Robber.py

--- LeetCode/Medium/House Robber/House Robber.py
+++ LeetCode/Medium/House Robber/House Robber.py
@@ -7,7 +7,9 @@ class Solution:
         if len(nums)==2:
             return max(nums[0],nums[1])
         
+        
         dp[0],dp[1],dp[2] = nums[0],nums[1],nums[2]
+        
         
         for i in range(2,len(nums)):
             dp[i] = max(dp[i-2],dp[i-3])+nums[i]
