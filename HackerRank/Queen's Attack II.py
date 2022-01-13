7e0157c94fba3fe7917f2e93277b7669c1434b7f
Yang Jaeho <42399580+SGTYang@users.noreply.github.com>
2022-01-13 13:56:17 +0900

    Update Queen's Attack II.py

--- HackerRank/Queen's Attack II.py
+++ HackerRank/Queen's Attack II.py
@@ -20,19 +20,14 @@ import sys
 
 def queensAttack(n, k, r_q, c_q, obstacles):
     # Write your code here
-    moves = [(1,0),(1,1),(-1,0),(-1,-1),(0,1),(0,-1)]
+    vset=set([tuple(item) for item in obstacles])
+    moves = [(1,0),(-1,0),(-1,-1),(0,-1),(0,1),(1,1),(1,-1),(-1,1)]
     ans = 0
-    print(obstacles)
-    
     for y,x in moves:
-        new_c_q, new_r_q = r_q + y, c_q + x
-        print(new_c_q, new_r_q)
-        while new_c_q >=0 and new_r_q >=0 and new_c_q<n and new_r_q<k:
-            if (new_c_q,new_r_q) in obstacles:
-                break
+        new_c_q, new_r_q = c_q+y, r_q + x
+        while new_c_q >=1 and new_r_q >=1 and new_c_q<=n and new_r_q<=n and (new_r_q,new_c_q) not in vset:
             ans += 1
             new_c_q, new_r_q = new_c_q+y, new_r_q+x 
-    
     return ans 
 
 if __name__ == '__main__':
