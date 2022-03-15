93578d6638fe290f1b322c31f62b52947fdc7a14
Yang Jaeho <42399580+SGTYang@users.noreply.github.com>
2022-03-15 21:36:41 +0900

    Create 자연수 뒤집어 배열로 만들기.go

---
+++ Programmers/Level1/자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.go
@@ -0,0 +1,13 @@
+import (
+    "strconv"
+)
+func solution(n int64) []int {
+    answer := []int{}
+    str := strconv.Itoa(int(n))
+
+    for i := len(str) - 1; i >= 0; i-- {
+        plus, _ := strconv.Atoi(string(str[i]))
+        answer = append(answer, plus)
+    }
+    return answer
+}
