7c5fb6cb818a28ccb89f52b07b8885a85b8932a5
Yang Jaeho <42399580+SGTYang@users.noreply.github.com>
2022-03-16 21:32:27 +0900

    Update 자연수 뒤집어 배열로 만들기.go

--- Programmers/Level1/자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.go
+++ Programmers/Level1/자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.go
@@ -4,7 +4,6 @@ import (
 func solution(n int64) []int {
     answer := []int{}
     str := strconv.Itoa(int(n))
-
     for i := len(str) - 1; i >= 0; i-- {
         plus, _ := strconv.Atoi(string(str[i]))
         answer = append(answer, plus)
