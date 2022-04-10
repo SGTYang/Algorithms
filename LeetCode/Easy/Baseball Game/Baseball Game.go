fa4ab043e125c0c8f9dc8dbd14f5f23c96406135
Yang Jaeho <42399580+SGTYang@users.noreply.github.com>
2022-04-10 19:16:04 +0900

    Create Baseball Game.go

---
+++ LeetCode/Easy/Baseball Game/Baseball Game.go
@@ -0,0 +1,23 @@
+func calPoints(ops []string) int {
+    container := make([]int,len(ops))
+    var ans int
+    
+    for _,v := range ops{
+        if v == "C"{
+            container = container[:len(container)-1]
+        }else if v == "D"{
+            container = append(container, container[len(container)-1]*2)
+        }else if v == "+"{
+            container = append(container, container[len(container)-1]+container[len(container)-2])
+        }else{
+            num, _ := strconv.Atoi(v)
+            container = append(container, num)
+        }
+    }
+    
+    for _, v:= range container{
+        ans += v
+    }
+    
+    return ans
+}
